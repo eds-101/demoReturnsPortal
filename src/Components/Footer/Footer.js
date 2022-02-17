d70dfccd74334c17315eb684f53631cea5c1f327
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-02-17 11:03:26 +0000

    skeletonHomepage

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+    // make footer text in centre
+    return(
+        <div className="fixed w-full bottom-0 left-0 bg-yellow-300 h-20">
+            <h4 className="text-lg text-center "
+            >Help and Support</h4>
+        </div>
+    )
+
+}
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+
+    // flex items-center justify-center bg-yellow-300 h-20
+        // <div className="Header items-center w-full">
+        //     <h1 className="text-5xl font-bold text-center bg-yellow-300 h-20"
+        //     >Returns Portal</h1>
+        // </div>
+
+    return(
+        <div className="flex items-center justify-center bg-yellow-300 h-20">
+            <h1 className="text-5xl font-bold"
+            >Returns Portal</h1>
+        </div>
+    )
+
+}

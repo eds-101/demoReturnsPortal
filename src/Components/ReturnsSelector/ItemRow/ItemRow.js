37ad10c83cf218f88c7e3e9b099005b247836b30
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-02-17 12:28:39 +0000

    ItemRow.js

---
+++ src/Components/ReturnsSelector/ItemRow/ItemRow.js
@@ -0,0 +1,58 @@
+const returnsReasons = [
+    "Does not fit", "I changed my mind", "It's too expensive"
+]  
+
+function ItemRow(props) { 
+
+    function populateReturnQuantityOptions(quantity) {
+        const quantList = []
+        for(let i=0; i <= quantity; i++) {
+            quantList.push(i)
+        }
+        return quantList
+    } 
+
+    const quantity = populateReturnQuantityOptions(props.quantity)
+
+    function returnReason(reason, id, name, imageURL){
+        let StoringReasonAndItemID = {'ItemId': id, 'Reason': reason.target.value, 'Name': name, 'ImageURL': imageURL, 'Quantity': 0}  
+        props.returnReasonHandler(StoringReasonAndItemID)
+    }
+
+    function returnQuantity(quantity, id, name, imageURL){
+        const itemQuantityToReturn = {'ItemId': id, 'Quantity': Number(quantity.target.value), 'Name': name, 'ImageURL': imageURL}
+        props.returnQuantityHandler(itemQuantityToReturn)  
+    }
+
+    return(
+        <div className='container'> 
+            <div className='left'>
+                <div className='itemPicture'>
+                    <img src={props.imgURL} alt=""/> 
+                </div>
+            </div> 
+            <div className='middle'>
+                <div className='returnQuantityMenu'>
+                    <h2>{props.name}</h2> 
+                        { props.returnable ? <label>How many do you want to return?</label> : null}
+                        { props.returnable ? <select required onChange={(e) => returnQuantity(e, props.itemID, props.name, props.ImageURL)}>
+                            {quantity.map(i => <option value={i}>{Number(i)}</option>)}
+                        </select> : null}
+                    </div> 
+                    { props.returnable ? null : <p>Item is outside the return perirod</p>}
+                </div> 
+            <div className='right'>
+                <div className='returnReasonsMenu'>
+                { props.returnable ? <label>Why are you returning this?</label> : null }
+                    { props.returnable ? <select onChange={(e) => returnReason(e, props.itemID, props.name, props.ImageURL)}>
+                        <option value="">Select a reason...</option>
+                        {returnsReasons.map((r) => <option value={r}>{r}</option>)}
+                    </select> : null }
+                </div>
+            </div>  
+            <hr />
+        </div>
+    )
+}
+
+export default ItemRow

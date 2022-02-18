e0f7954a6b97e483d04a6e6bca6d3d97d62aaa57
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-02-18 16:04:31 +0000

    basicReturnsComplete

--- src/Components/ReturnsComplete/ReturnsComplete.js
+++ src/Components/ReturnsComplete/ReturnsComplete.js
@@ -4,21 +4,21 @@ function ReturnsComplete(props){
 
     return(
         <div>
-            <div className='headline'>
-                <h2>
-                    Your return has been completed
-                </h2>
-                <h4>
-                    Confirmation of returned products:
-                </h4>
-            </div>
-            <div className='body'>
+            <div class="bg-[url('https://i.imgur.com/jAXaawT.jpg')] h-screen bg-cover bg-center flex justify-items-center items-center">
+                <div class="px-10 lg:px-32 xl:px-40">
+                <h1 class="text-6xl font-semibold mb-6">
+                    <span>Return Complete</span>
+                </h1>
                 <ul>
                     {props.finalOrder.map((product) => {
-                       return <li>{` ${product['Quantity'] > 1 ? product['Quantity'] + 'x' : ""}`} {product['Name'].slice(0,1) + product['Name'].slice(1).toLowerCase()}</li>
+                       return <li class="text-lg max-w-md">
+                       {` ${product['Quantity'] > 1 ? product['Quantity'] + 'x' : ""}`} 
+                       {product['Name'].slice(0,1) + product['Name'].slice(1).toLowerCase()}</li>
                     })}
                 </ul>
-                <button className='button' onClick={goHome}>Back to Returns page</button>
+                <button onClick={goHome} class="inline-block mt-10 px-10 py-3 bg-yellow-300 text-lg text-black font-semibold"
+                >Back to Returns Page</button>
+                </div>
             </div>
         </div>
     )
